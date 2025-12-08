3ace437a7d60fd8c7f0da02ced2696d8af61e07a
jtranberg <jtranberg@hotmail.ca>
2025-12-07 21:16:07 -0800

    pdf

--- src/config/links.js
+++ src/config/links.js
@@ -1,11 +1,12 @@
 const LINKS = {
+   resumePdf: "/Jay_Tranberg_Resume.pdf",
+  appendixPdf: "/Jay_Tranberg_Appendix.pdf",
   linkedin: "https://www.linkedin.com/in/jaytranberg",
   email: "mailto:jtranberg@hotmail.ca",
   tel: "tel:+12505161137",
   soundscapes: "https://play.google.com/store/apps/details?id=com.jaytranberg.soundscapeschill",
   inAndOut: "https://theinandoutapp.com/",
-  resumePdf: "/assets/jay_resume_with_metrics_ascii_v3.pdf",
-  appendixPdf: "/assets/jay_project_appendix_ascii_v3.pdf",
+ 
 };
 
 export default LINKS;
